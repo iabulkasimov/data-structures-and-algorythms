6f1c73238c99a12a76b0bca9dedf7d97da59964d
iabulkasimov <iabulkasimov@gmail.com>
2022-04-20 18:49:38 -0400

    Data Structure

--- DataStructures-01-Arrays/src/RotateMatrixMain.java
+++ DataStructures-01-Arrays/src/RotateMatrixMain.java
@@ -1,55 +1,14 @@
 public class RotateMatrixMain {
     public static void main(String[] args) {
-        int [][] matrix = new int[5][5];
-        int valueOfCell = 1;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = valueOfCell++;
-            }
-        }
-        printMatrix(rotateMatrixRight(matrix));
-    }
+        int[][] matrix = new int[3][3];
+        int pixValue=1;
+        for (int i=0;i<3;i++)
+            for (int y=0;y<3;y++)
+                matrix[i][y]=pixValue++;
 
-    public static int[][] rotateMatrixRight(int [][] matrix){
-        if (!(matrix.length == 0 || matrix.length != matrix[0].length)) {
-            int n = matrix.length;
 
-            for (int layer = 0; layer < n/2; layer++) {
-                int first = layer;
-                int last = n - 1 - layer;
-                System.out.println("Initial Matrix at Layer: " + layer);
-                printMatrix(matrix);
-                for (int i = first; i < last; i++) {
-                    int offset = i - first;
-                    int temp = matrix[first][i]; //save top to temp
-                    //printMatrix(matrix);
-                    //left -> top
-                    matrix[first][i] = matrix[last - offset][first];
-                    //printMatrix(matrix);
-                    //bottom -> left
-                    matrix[last - offset][first] = matrix[last][last - offset];
-                    //printMatrix(matrix);
-                    //right -> bottom
-                    matrix[last][last - offset] = matrix[i][last];
-                    //printMatrix(matrix);
-                    //top -> right
-                    matrix[i][last] = temp; //right <- saved temp value
-                    printMatrix(matrix);
-                }
-            }
-        }
-        return matrix;
-    }
-
-    public static void printMatrix(int[][] Arr){
+        MatrixService.rotate90(matrix);
         System.out.println();
-        System.out.println("------------");
-        for (int[] arr:Arr) {
-            System.out.println("    ");
-            for (int number:arr) {
-                System.out.print(number + ",");
-            }
-//            System.out.println("");
-        }
+        MatrixService.printMatrix(matrix);
     }
 }
